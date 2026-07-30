d368a2b6142e872fd9a4e117082a027570411763
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-07-30 12:45:27 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to the java track.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("My name is mahesh babu.");
+        System.out.println("I goal is to become a java developer.");
     }
 }
