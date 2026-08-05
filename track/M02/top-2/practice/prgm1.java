2f6860db6010c4fc2832059bf88d9a4b196e90a3
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-05 12:00:05 +0530

    file 1

---
+++ track/M02/top-2/practice/prgm1.java
@@ -0,0 +1,10 @@
+
+public class prgm1 {
+
+    public static void main(String[] args) {
+        int marks = 80;
+        String res = marks >= 60 ? "pass" : "fail";
+        System.out.println(res);
+
+    }
+}
