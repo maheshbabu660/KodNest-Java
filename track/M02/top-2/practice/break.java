f677ab1f608dca792b386b818a037a29a42036c0
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-10 08:18:11 +0530

    Add practice programs

---
+++ track/M02/top-2/practice/break.java
@@ -0,0 +1,14 @@
+
+public class breakpgm {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+
+            if (i == 6) {
+                break;
+            }
+        }
+    }
+}
