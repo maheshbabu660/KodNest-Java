2aea1b7f7d68548f5e190755336bae371ec64605
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-10 08:59:48 +0530

    Add practice programs

---
+++ track/M02/top-2/practice/labelledcontinue.java
@@ -0,0 +1,16 @@
+
+public class labelledcontinue {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 3) {
+                    continue outer;
+                }
+                System.out.println("i:" + i + " j:" + j);
+            }
+        }
+    }
+}
