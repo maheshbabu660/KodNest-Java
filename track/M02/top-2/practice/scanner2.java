43514d24e29c2ae25b784e0d8c41ca8311a6aa1f
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-10 21:24:23 +0530

    Add practice programs

---
+++ track/M02/top-2/practice/scanner2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class scanner2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.err.println("Enter your age : ");
+        int age = scan.nextInt();
+        System.out.println("Age is :" + age);
+
+        System.out.println("Enter your height:");
+        float height = scan.nextFloat();
+        System.out.println("Height is:" + height);
+
+        scan.nextLine();
+        System.out.println("Enter your full name: ");
+        String fullname = scan.nextLine();
+        System.out.println("Full name: " + fullname);
+
+    }
+}
