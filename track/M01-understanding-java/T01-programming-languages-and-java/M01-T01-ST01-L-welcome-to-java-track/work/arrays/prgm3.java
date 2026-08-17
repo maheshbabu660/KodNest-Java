738aeac94fb1cb52615a59bbd776310fb8239b82
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-17 13:07:29 +0530

    Clean up loop bounds and add scanner closing in array programs

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm3.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm3.java
@@ -8,14 +8,16 @@ public class prgm3 {
 
         System.out.println("Enter array elements");
 
-        for (int i = 0; i <= a.length - 1; i++) {
+        for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
 
-        System.out.println("Array elements are ");
+        System.out.println("Array elements are:");
 
-        for (int i = 0; i <= a.length - 1; i++) {
+        for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+
+        sc.close();
     }
 }
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm4.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm4.java
@@ -17,5 +17,7 @@ public class prgm4 {
         for (int i = a.length - 1; i >= 0; i--) {
             System.out.println(a[i]);
         }
+
+        sc.close();
     }
 }
