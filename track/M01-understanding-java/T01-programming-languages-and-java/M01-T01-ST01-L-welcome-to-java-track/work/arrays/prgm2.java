41c989b4fda50f198f7fc0fc09e1f71292c43bf6
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-17 12:08:40 +0530

    Add prgm1.java and prgm2.java in arrays

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm2.java
@@ -0,0 +1,14 @@
+
+
+public class prgm2 {
+    public static void main(String[] args) {
+
+        int[] arr={14,50,37,470,580};
+
+            System.out.println(arr[0]);
+            System.out.println(arr[1]);
+            System.out.println(arr[2]);
+            System.out.println(arr[3]);
+            System.out.println(arr[4]);
+    }
+}
