8e817db919e550e0f882515e5f3ed6124b501266
pyneni mahesh babu <maheshpyneni63@gmail.com>
2026-08-17 13:00:03 +0530

    Fix and update all array folder programs

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/arrays/prgm4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class prgm4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a[] = new int[5];
+
+        System.out.println("Enter array elements");
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements in reverse order");
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
